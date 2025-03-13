5e942fd7f1b0e5fae683bd23b80346b3e2e3eb45
Sandeep <sandygeetha.6189@gmail.com>
2025-03-13 17:18:06 +0530

    you

---
+++ com.TY.JAVAsession/src/test/java/practice_java/Java.java
@@ -0,0 +1,29 @@
+package practice_java;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Java 
+{
+	//@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+		driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name']")).click();
+		driver.findElement(By.xpath("//a[text()='Logout']")).click();
+		System.out.println("pass");
+		driver.quit();
+		
+		
+	}
+
+}
