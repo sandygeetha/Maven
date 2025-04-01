10d95fba80b0bf034372ee83e419f06255842433
Sandygeetha <sandygeetha.6189@gmail.com>
2025-04-01 22:21:47 +0530

    test

---
+++ com.TY.JAVAsession/src/test/java/practice_java/RemoveSpace.java
@@ -0,0 +1,15 @@
+package practice_java;
+
+public class RemoveSpace {
+
+	public static void main(String[] args) 
+	{
+		//     public static void main(String[] args) {
+        String name = "sandeep cs";  // Example with space
+        String result = name.replace(" ", ""); // Removing spaces
+        System.out.println("Original String: " + name);
+        System.out.println("String after removing spaces: " + result);
+ 
+	}
+
+}
